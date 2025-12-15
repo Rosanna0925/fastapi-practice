1d23287f267b8c3d8734a01b515d4f0e874e7779
RosannaCLChang <RosannaCLChang@fih-foxconn.com>
2025-12-15 16:50:21 +0800

    add simple movie schema

--- src/main.py
+++ src/main.py
@@ -6,7 +6,26 @@ from pydantic import BaseModel
 
 Base=declarative_base()
 
+class Movielist(Base):
+    __tablename__="movies"
 
+    id=Column(Integer, primary_key=True, index=True)
+    name=Column(String, primary_key=True, index=True)
+    rating=Column(Integer, index=True)
+    comment=Column(String, index=True)
+    intro=Column(String, index=True)
+
+SQLALCHEMY_DATABASE_URL="sqlite:///./test.db"
+engine=create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal=sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base.metadata.create_all(bind=engine)
+
+class Movie(BaseModel):
+    name: str
+    rating: int
+    comment: str
+    intro: str
 
 app=FastAPI()
 
