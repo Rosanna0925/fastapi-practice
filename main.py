4ef4f23f03b92f296becd31f50a0a9cccfc51aef
RosannaCLChang <RosannaCLChang@fih-foxconn.com>
2025-12-19 17:09:35 +0800

    WIP

--- main.py
+++ main.py
@@ -6,24 +6,29 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
 from pydantic import BaseModel
 
-Base=declarative_base()
-
-class MovieDB(Base):
-    __tablename__="movies"
-
-    id=Column(Integer, primary_key=True, index=True)
-    name=Column(String, primary_key=True, index=True)
-    intro=Column(String, index=True)
-    rating=Column(Integer, index=True)
-
 SQLALCHEMY_DATABASE_URL="sqlite:///./test.db"
 engine=create_engine(SQLALCHEMY_DATABASE_URL)
 Sesssional=sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-class Movie(BaseModel):
-    name:str
-    intro:str
-    rating:int
+Base=declarative_base()
+
+class Movie(Base):
+    __tablename__="movies"
+
+    id=Column(Integer, primary_key=True, index=True)
+    name=Column(String, index=True)
+    intro=Column(String, index=True)
+    rating=Column(Integer, index=True)
+
+Base.metadata.create_all(bind=engine)
+
+
+movie_1=Movie(name="My sister's host", intro="touched movie!", rating=6)
+
+with Session(engine) as session:
+    session.add(movie_1)
+    session.commit()
+    session.refresh(movie_1)
 
 app=FastAPI()
 
@@ -31,5 +36,6 @@ templates=Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
+    movies=Movie.query.all()
     return templates.TemplateResponse(
-        request=request, name="index.html")
+        request=request, name="index.html", movies=movies)
