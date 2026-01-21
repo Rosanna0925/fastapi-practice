6910badf7dc0d3839affc85794f72ec4ec8c655f
Rosanna <RosannaCLChang@fih-foxconn.com>
2026-01-21 22:21:42 +0800

    implement environment security

--- database.py
+++ database.py
@@ -2,8 +2,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PASSWORD")
+db_host = os.getenv("DB_HOST", "localhost")  # 如果沒設定就用 localhost
+db_port = os.getenv("DB_PORT", "5432")
+db_name = os.getenv("DB_NAME")
 
-SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://dev_user:dev_password123@localhost:5432/fastapi_test_db"
+SQLALCHEMY_DATABASE_URL = f"postgresql+psycopg2://{dev_user}:{db_password}@{db_host}:{db_port}/{db_name}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
